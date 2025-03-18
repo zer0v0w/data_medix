df63db5dd22f27e4390a7a6be90eaf714e8596bb
zer0v0w <iam.zer0v0@gmail.com>
2025-03-18 22:11:41 +0200

    just made the header section

---
+++ data_medix/lib/colors.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+
+bool darkMode = false;
+
+void toggleDarkMode(){
+  darkMode = !darkMode;
+  colorsList = darkMode? {
+
+  "praimryColor": Color.fromRGBO(0, 75, 200, 1),
+  "secondaryColor": Color.fromRGBO(200, 25, 200, 1),
+  "accentColor": Color.fromRGBO(70, 140, 255, 1),
+  "backgroundColor": Color.fromRGBO(30, 30, 30, 1),
+  "praimrytext": Color.fromRGBO(255, 255, 255, 1),
+  "secondarytext": Color.fromRGBO(180, 180, 180, 1),
+  "powercolor":Color.fromRGBO(255, 0, 175, 1),
+  "dividerColor":Color.fromRGBO(200, 200, 200, 1),
+}:{
+ "praimryColor": Color.fromRGBO(0, 75, 175, 1),
+  "secondaryColor": Color.fromRGBO(200, 25, 200, 1),
+  "accentColor": Color.fromRGBO(70, 140, 255, 1),
+  "backgroundColor": Color.fromRGBO(255, 255, 255, 1),
+  "praimrytext": Color.fromRGBO(20, 20, 20, 1),
+  "secondarytext": Color.fromRGBO(117, 117, 117, 1),
+  "powercolor":Color.fromRGBO(50, 0, 255, 1),
+  "dividerColor":Color.fromRGBO(210, 210, 210, 1),
+
+};
+}
+
+Map <String,Color> colorsList = darkMode? {
+
+  "praimryColor": Color.fromRGBO(0, 75, 200, 1),
+  "secondaryColor": Color.fromRGBO(200, 25, 200, 1),
+  "accentColor": Color.fromRGBO(70, 140, 255, 1),
+  "backgroundColor": Color.fromRGBO(30, 30, 30, 1),
+  "praimrytext": Color.fromRGBO(255, 255, 255, 1),
+  "secondarytext": Color.fromRGBO(180, 180, 180, 1),
+  "powercolor":Color.fromRGBO(255, 0, 175, 1),
+  "dividerColor":Color.fromRGBO(200, 200, 200, 1),
+}:{
+ "praimryColor": Color.fromRGBO(0, 75, 175, 1),
+  "secondaryColor": Color.fromRGBO(200, 25, 200, 1),
+  "accentColor": Color.fromRGBO(70, 140, 255, 1),
+  "backgroundColor": Color.fromRGBO(255, 255, 255, 1),
+  "praimrytext": Color.fromRGBO(20, 20, 20, 1),
+  "secondarytext": Color.fromRGBO(117, 117, 117, 1),
+  "powercolor":Color.fromRGBO(50, 0, 255, 1),
+  "dividerColor":Color.fromRGBO(210, 210, 210, 1),
+
+};
--- data_medix/lib/main.dart
+++ data_medix/lib/main.dart
@@ -1,20 +1,158 @@
 import 'package:flutter/material.dart';
+import 'colors.dart';
+import 'package:simple_gradient_text/simple_gradient_text.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
-  runApp(const MainApp());
+Future<void> main() async {
+  await Supabase.initialize(
+    url: 'https://aaxnfbigquknldsmhhrb.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFheG5mYmlncXVrbmxkc21oaHJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE0NjUxMDcsImV4cCI6MjA1NzA0MTEwN30.4-yUtxpz8CztBB5_Hg_nwdCzC9RUSNMvMNSuLY-qI9U',
+  );
+
+  runApp(MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  void test(){
+    setState(() {
+      toggleDarkMode();
+    });
+  }
+  @override
+  
+
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        backgroundColor: colorsList["backgroundColor"],
+        body: Headtop(toggle :test),
       ),
     );
   }
 }
+
+//logo
+class Logo extends StatelessWidget {
+  const Logo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    bool isweb = width > 800;
+
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Container(
+            margin: EdgeInsets.fromLTRB(
+                isweb ? width * 0.01 : width * 0.03, height * 0.012, 0, 0),
+            width: isweb ? width * 0.034 : width * 0.08,
+            height: height * 0.07,
+            child: Image.asset('lib/assets/logo.jpg')),
+        GradientText(
+          'Data medix',
+          style: TextStyle(
+            fontSize: isweb ? height * 0.025 : height * 0.02,
+          ),
+          colors: [
+            colorsList['praimryColor']!,
+            colorsList['secondaryColor']!,
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+//search bar
+class SearchBar extends StatefulWidget {
+  const SearchBar({super.key});
+
+  @override
+  State<SearchBar> createState() => _SearchBarState();
+}
+
+class _SearchBarState extends State<SearchBar> {
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    bool isweb = width > 800;
+
+    return Container(
+      width: width * 0.25,
+      height: height * 0.04,
+      decoration: BoxDecoration(
+          color: colorsList["dividerColor"],
+          borderRadius: BorderRadius.circular(35)),
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: Icon(Icons.search),
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.only(bottom: height * 0.01),
+              child: TextField(
+                textAlign: TextAlign.left,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'Search',
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+//headtop
+class Headtop extends StatefulWidget {
+  const Headtop({super.key,  required this.toggle});
+  final void Function() toggle;
+  @override
+  State<Headtop> createState() => _HeadtopState();
+}
+
+class _HeadtopState extends State<Headtop> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Logo(),
+        TextButton(
+            onPressed: () {
+              setState(() {
+               widget.toggle();
+              });
+            },
+            child: Text(
+              darkMode ? "light" : "dark",
+              style: TextStyle(color: colorsList["praimrytext"]),
+            )),
+        Spacer(
+          flex: 1,
+        ),
+        SearchBar(),
+        Spacer(
+          flex: 2,
+        ),
+      ],
+    );
+  }
+}
+
+
+//card
